c912aa219222053d23684f8b6b0f48bea2ece2b5
Quyengithul <96811508+Quyengithul@users.noreply.github.com>
2022-04-18 09:18:52 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
-var array=['Hello Yaakov','Good Bye John','Good Bye Jen','Good Bye Jason','Hello Paul','Hello Frank','Hello Larry','Hello Paula','Hello Laura','Good Bye Jim',];
+var array=['Hello Yaakov',
+'Good Bye John',
+'Good Bye Jen',
+'Good Bye Jason',
+'Hello Paul',
+'Hello Frank',
+'Hello Larry',
+'Hello Paula',
+'Hello Laura',
+'Good Bye Jim',
+];
 
 for(var i=0;i<9;i++)
 {
