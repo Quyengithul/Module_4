a1d86455e6374dc77ad52eb747d0de764300977a
Quyengithul <96811508+Quyengithul@users.noreply.github.com>
2022-04-18 09:07:06 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,6 @@
+var array=['Hello Yaakov','Good Bye John','Good Bye Jen','Good Bye Jason','Hello Paul','Hello Frank','Hello Larry','Hello Paula','Hello Laura','Good Bye Jim'];
 
-console.log('Hello Yaakov')
-
-console.log('Good Bye John')
-
-console.log('Good Bye Jen')
-
-console.log('Good Bye Jason')
-
-console.log('Hello Paul')
-
-console.log('Hello Frank')
-
-console.log('Hello Larry')
-
-console.log('Hello Paula')
-
-console.log('Hello Laura')
-
-console.log('Good Bye Jim')
+for(var i=0;i<9;i++)
+{
+console.log(array[[i]);
+}
